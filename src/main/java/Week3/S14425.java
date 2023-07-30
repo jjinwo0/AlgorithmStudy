92a479cb63b52d5921228ee0d7debb3287dcdd6b
Jinwoo <zvzv9808@gmail.com>
2023-07-30 18:07:06 +0900

    Week4: Stack

--- src/main/java/Week3/S14425.java
+++ src/main/java/Week3/S14425.java
@@ -1,8 +1,10 @@
+package Week3;
+
 import java.io.*;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
-public class Main {
+public class S14425 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
